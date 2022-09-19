ba74cd1ee582311b3270ad9a040ede97b6b885e1
Aboufazel <a.ab2521@gmail.com>
2022-09-19 20:11:51 +0430

    add save btn for the list

--- src/App.js
+++ src/App.js
@@ -9,23 +9,35 @@ function App() {
   const [form , setForm] = useState([
       {title:'' , dec:'' , state:false}
   ])
+   const [status , setStatus] = useState(['add'])
 
   const mangeDelete = id =>{
       setState(state.filter(todo => todo.id !== id));
   }
-
+  const manageSubmit = e =>{
+      e.preventDefault();
+      setState([...state , {id: Math.floor(Math.random()*1000) , title: form.title , des: form.des , state: false}]);
+      setForm({title:'' , dec:''});
+  }
   const mangeEdit = e =>{
       setForm({...form, [e.target.name]: e.target.value})
   }
   return (
       <div>
-          <form></form>
+          <form onSubmit={manageSubmit}>
+              <label htmlFor={'title'}>Title:</label>
+              <input type={'text'} name={'des'} onChange={mangeEdit}/>
+              <label htmlFor={'des'}>Des:</label>
+              <input type={'text'} name={'des'} onChange={mangeEdit}/>
+              <button type={'submit'} style={{background: 'darkgreen' , border:0 , color:'white', marginLeft:10 , padding:4 , borderRadius:5}}>
+                  save
+              </button>
+          </form>
           {state.map(todo =>(
               <div style={{marginTop:25 , border: "1px solid black" , margin:50 , borderRadius:5 ,padding:25}}>
-                  <div>des: {todo.des}</div>
-                  <div>id: {todo.id}</div>
-                  <div>title: {todo.title}</div>
-                  <div>id: {todo.id}</div>
+                  <h3>id: {todo.id}</h3>
+                  <h4>title: {todo.title}</h4>
+                  <h5>des: {todo.des}</h5>
                   <button style={{background:"darkred" ,color:"white" , border:0 , padding:"4px 12px 4px 12px" , borderRadius:5 , marginRight:5}} onClick={()=>mangeDelete(todo.id)}>Delete</button>
                   <button style={{background:"goldenrod", border:0 , padding:"4px 12px 4px 12px" , borderRadius:5 , marginRight:5}} onClick={()=>mangeEdit(todo)}>Edit</button>
               </div>
